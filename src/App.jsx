f16b1fefc4729582477738a0d8c0cfc48e4a5eb0
Lucas Beley-Titah <lucas.beley@hotmail.fr>
2025-07-18 17:34:37 +0200

    Apps section with descriptions and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div id="page-container">
         <div id="bio">
           <div className="in-container">
             <img src={"/profile-pic.jpg"} alt="Lucas Beley-Titah" />
-            <h1>
-              Lucas Beley-Titah
-            </h1>
+            <h1>Lucas Beley-Titah</h1>
           </div>
         </div>
 
-        <div id="separator"></div>
+        <div className="vertical-separator"></div>
 
         <div id="links">
           <div className="in-container">
-            <a target="_blank" href="https://unagi.lucasbt.fr">
-              <img src="/icons8-blog-96.png" />
-              <span>Blog</span>
-            </a>
             <a target="_blank" href="https://github.com/LucasBeley">
               <img src="/icons8-github.svg" />
               <span>GitHub</span>
             </a>
-            <a target="_blank" href="https://www.linkedin.com/in/lucas-beley-titah-726507128">
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/lucas-beley-titah-726507128"
+            >
               <img src="/icons8-linkedin.svg" />
               <span>LinkedIn</span>
             </a>
@@ -36,6 +33,23 @@ function App() {
             <a href="mailto:lucas.beley@hotmail.fr">
               <img src="/icons8-mail-96.png" />
               <span>Email</span>
+            </a>
+          </div>
+          <div className="horizontal-separator"></div>
+          <div className="in-container">
+            <a target="_blank" href="https://unagi.lucasbt.fr">
+              <img src="/icons8-blog-96.png" />
+              <div className="title-desc-container">
+                <span>Unagi</span>
+                <p>A blog built with Django.</p>
+              </div>
+            </a>
+            <a target="_blank" href="https://pomo.lucasbt.fr">
+              <img src="/icons8-pomodoro-96.png" />
+              <div className="title-desc-container">
+                <span>Pomo</span>
+                <p>A pomodoro timer built with Svelte.</p>
+              </div>
             </a>
           </div>
         </div>
@@ -52,5 +66,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
