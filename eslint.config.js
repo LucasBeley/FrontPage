540390489405ee6c59fb85047b76ff08419548b0
LucasBeley <lucas.beley@hotmail.fr>
2026-07-13 06:54:30 +0000

    chore: update deps to latest, migrate to React 19 + ESLint 10 flat config

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  { ignores: ['dist'] },
+  js.configs.recommended,
+  reactRefresh.configs.vite,
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: { 'react-hooks': reactHooks },
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    rules: reactHooks.configs['recommended-latest'].rules,
+  },
+]
